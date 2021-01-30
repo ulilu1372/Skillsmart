be60bdcd71276f02d322a42ccc1fc50b3bf90f6f
ulilu1372 <ulilu1372@mail.ru>
2021-01-30 18:00:22 +0400

    Create mytest2.py

---
+++ mytest2.py
@@ -0,0 +1,13 @@
+def odometer(oksana):
+    a = 0
+    b = 0
+    c = 0
+    for i in range(len(oksana)):
+        if i % 2 == 0:
+            a = list[i]
+        if i % 2 != 0 and i != 1:
+            b = oksana[i] - oksana[i - 2]
+            c += a * b
+        if i == 1:
+            b = oksana[i]
+            c += a * b
