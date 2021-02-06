1a4acf6dfec027f1fcdecb4e64c7b33f4bd0b017
ulilu1372 <ulilu1372@mail.ru>
2021-02-06 15:40:13 +0400

    Create mytest3.py

---
+++ mytest3.py
@@ -0,0 +1,21 @@
+def ConquestCampaign(N, M, L, battalion):
+    counter = 1
+    battalion_new = []
+    list = []
+    for i in range(len(battalion)):
+        if i % 2 != 0:
+            battalion_new.append([battalion[i - 1], battalion[i]])
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            list.append([i,j])
+    while list != []:
+        for i in range(len(battalion_new)):
+            battalion_new.append([battalion_new[i][0] + 1, battalion_new[i][1]])
+            battalion_new.append([battalion_new[i][0], battalion_new[i][1] + 1])
+            battalion_new.append([battalion_new[i][0], battalion_new[i][1] - 1])
+            battalion_new.append([battalion_new[i][0] - 1, battalion_new[i][1]])
+        counter += 1
+        for a in range(len(battalion_new)):
+            if battalion_new[a] in list:
+                list.remove(battalion_new[a])
+    return counter
