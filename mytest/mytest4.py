aa4fe218d2688234a7879250a8036346a2ba0749
ulilu1372 <ulilu1372@mail.ru>
2021-02-06 18:26:20 +0400

    Delete mytest directory

--- mytest/mytest4.py
+++
@@ -1,6 +0,0 @@
-def MadMax(N, Tele):
-    a = sorted(Tele)[:len(Tele)//2]
-    b = sorted(Tele, reverse=True)[1 : len(Tele)//2 + 1]
-    c = sorted(Tele, reverse=True)[0]
-    a.append(int(c))
-    return a + b
