f3aa44785de2756e97df1eccf4a4a8dbe240b70a
ulilu1372 <ulilu1372@mail.ru>
2021-02-22 12:23:43 +0400

    Update mytest7.py

--- mytest7.py
+++ mytest7.py
@@ -1,8 +1,8 @@
 def WordSearch(widht_str, s, subs):
-    court = 0  # счетчик длинны предидущей строки
-    court1 = 0  # счетчик операци за один проход на более widht_str
-    court_all = 0  # общий счетчик операцииъ
-    court_speace = 0  # добавляем пропушенный пробел в строке 13
+    court = 0  
+    court1 = 0  
+    court_all = 0  
+    court_speace = 0  
     add_str = ''
     sum_arr = []
     line_length = 0
@@ -10,16 +10,16 @@ def WordSearch(widht_str, s, subs):
     len_frases = len(s)
     for i in range(len_frases):
         while j < len(s) - 1:
-            if s[court] == ' ':  # проскакиваем пробел начинаем подсчет со след позиции(с символа)
+            if s[court] == ' ':  
                 court += 1
-                court_speace = 1  # добавляем пропущенный пробел в счетчик line_lenght
-                # бегаем по строке необходимое количество отрезков
+                court_speace = 1  
+                
             for j in range(0 + court,
-                           widht_str + court):  # перебебираем символы и строке заданными диапазонами widht_str
+                           widht_str + court):  
                 court_all += 1
                 court1 += 1
-                add_str += s[j]  # коктенация строки
-                if j == len(s) - 1:  # добавляем последнее слово
+                add_str += s[j]  
+                if j == len(s) - 1:  
                     sum_arr.append(add_str)
                     break
                 if widht_str == court1:
@@ -27,15 +27,15 @@ def WordSearch(widht_str, s, subs):
                         sum_arr.append(add_str)
                     if add_str.find(' ') != -1 and s[
                         (line_length + court1)] != ' ':
-                        for k in range(len(add_str) - 1, -1, -1):  # перебираем строку с конца до пробела
-                            if add_str[k] == ' ':  # проверяем не закинчивается ли строка
-                                add_str = add_str[0:k]  # пробелом или следующий символ не пробел ли?
+                        for k in range(len(add_str) - 1, -1, -1):  
+                            if add_str[k] == ' ':  
+                                add_str = add_str[0:k]  
                                 sum_arr.append(add_str)
                                 break
                     if s[(line_length + court1)] == ' ':
                         sum_arr.append(add_str)
                     line_length += len(
-                        add_str) + court_speace  # длинна текущей строки и добавляем пробелы которые пропустили
+                        add_str) + court_speace  
                     court += len(add_str)
                     add_str = ''
                     court1 = 0
