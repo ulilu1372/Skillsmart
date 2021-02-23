3217144c71885001db27f4d5b63b962b3b71c659
ulilu1372 <ulilu1372@mail.ru>
2021-02-23 20:32:58 +0400

    Update mytest8.py

--- mytest8.py
+++ mytest8.py
@@ -1,6 +1,29 @@
 def SumOfThe(N, data):
-    sum = 0
-    for i in range(len(data) - 1):
-        sum += data[i]
-    
-    return sum
+           
+    for i in range(1):
+            sum = 0
+            new_data = []
+            new_data = data[1:len(data)]
+            for j in range(len(new_data)):
+                sum += new_data[j]
+            if data[i] == sum:
+                return data[i]
+                
+    for i in range(1, len(data) - 1):
+            sum = 0
+            new_data = []
+            new_data = data[:i] + data[i + 1:]
+            for j in range(len(new_data)):
+                sum += new_data[j]
+            if data[i] == sum:
+                return data[i]           
+                
+        
+    for i in range(len(data) - 1, len(data)):
+            sum = 0 
+            new_data = []
+            new_data = data[0:len(data) - 1]
+            for j in range(len(new_data)):
+                sum += new_data[j]
+            if data[i] == sum:
+                return data[i]
