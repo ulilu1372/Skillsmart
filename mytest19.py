a4aea6bb0e0d6cefc2ede3a6ccaaed6949ff0040
ulilu1372 <ulilu1372@mail.ru>
2021-06-05 23:09:41 +0400

    Update mytest19.py

--- mytest19.py
+++ mytest19.py
@@ -1,1 +1,19 @@
+  
+def ShopOLAP(n,items):
+  result = []
+  stItems = {}
 
+  for i in range(len(items)):
+    item = items[i].split()
+    if stItems.get(item[0]) == None:
+      stItems[item[0]] = int(item[1])
+    else:
+      stItems[item[0]] += int(item[1])
+
+  srItem = sorted(stItems.items())
+  srItem.sort(key=lambda numb: numb[1], reverse=True)
+
+  for i in srItem:
+    result.append(i[0] + ' ' + str(i[1]))
+
+  return result
