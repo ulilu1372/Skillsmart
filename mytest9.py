01091186eacd579fc7620f9441f14aa44d9e079b
ulilu1372 <ulilu1372@mail.ru>
2021-06-08 18:46:59 +0400

    Update mytest9.py

--- mytest9.py
+++ mytest9.py
@@ -1,38 +1,38 @@
-#def StringRabbitsFoot(s, encode):
-encode = False
-s = 'отдай мою кроличью лапку'
-new_s = s.replace(' ', '')
-l = len(new_s)
-matrix = []
-new_matrix = []
-sqrt = l ** 0.5
-rows = int(sqrt)
-columns = round(sqrt)
-while l > rows * columns:
-    rows += 1
-for x in range(0, len(new_s), columns):
-    matrix.append(new_s[x:x+columns])
-for i in range(columns):       
-    if i == len(matrix[rows - 1]) - 1:
-        for j in range(rows):
-            new_matrix.append(matrix[j][i])
-        new_matrix.append(' ')
-    else: 
-        for j in range(rows - 1):
-            new_matrix.append(matrix[j][i])
-        new_matrix.append(' ')
+def StringRabbitsFoot(s, encode):
+    if encode == True:
+        new_s = s.replace(' ', '')
+        l = len(new_s)
+        matrix = []
+        new_matrix = []
+        sqrt = l ** 0.5
+        rows = int(sqrt)
+        columns = round(sqrt)
+        while l > rows * columns:
+            rows += 1
+        for x in range(0, len(new_s), columns):
+            matrix.append(new_s[x:x+columns])
+        for i in range(columns):       
+            if i == len(matrix[rows - 1]) - 1:
+                for j in range(rows):
+                    new_matrix.append(matrix[j][i])
+                new_matrix.append(' ')
+            else: 
+             for j in range(rows - 1):
+                new_matrix.append(matrix[j][i])
+             new_matrix.append(' ')
+        new_matrix = ''.join(new_matrix)
+        return new_matrix
 
-
-new_matrix = ''.join(new_matrix)
-print(new_matrix)
-
-new_matrix = 'омоюу толл дюиа акчп йрьк'
-length = len(new_matrix.replace(" ", ""))
-new_matrix = new_matrix.split()
-spisok = []
-for i in range(len(new_matrix)):
-    for j in range(len(new_matrix)):
-        spisok.append(new_matrix[j][i])
-        if len(spisok) == length:
-            break
-print(''.join(spisok))
+    if encode == False:
+        new_matrix = s
+        length = len(new_matrix.replace(" ", ""))
+        new_matrix = new_matrix.split()
+        spisok = []
+        for i in range(len(new_matrix)):
+            for j in range(len(new_matrix)):
+                spisok.append(new_matrix[j][i])
+                if len(spisok) == length:
+                    break
+        spisok = ''.join(spisok)
+        
+        return spisok
